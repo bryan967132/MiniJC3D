e1936fa736d0b6d3e3a0b0a095e1c803f694b644
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-23 02:15:47 -0600

    [Update] Parser

--- src/Language/Parser.java
+++ src/Language/Parser.java
@@ -2134,7 +2134,7 @@ class CUP$Parser$actions {
 		int idleft = ((java_cup.runtime.Symbol)CUP$Parser$stack.peek()).left;
 		int idright = ((java_cup.runtime.Symbol)CUP$Parser$stack.peek()).right;
 		String id = (String)((java_cup.runtime.Symbol) CUP$Parser$stack.peek()).value;
-		RESULT = new AccessID(idleft, idright, id);
+		RESULT = new AccessVar(idleft, idright, id);
               CUP$Parser$result = parser.getSymbolFactory().newSymbol("EXP",5, ((java_cup.runtime.Symbol)CUP$Parser$stack.peek()), ((java_cup.runtime.Symbol)CUP$Parser$stack.peek()), RESULT);
             }
           return CUP$Parser$result;
